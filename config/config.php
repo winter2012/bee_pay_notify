03f28e465fc8de922a0a36544f3c1a273788d864
winter2012 <winter2012102@gmail.com>
2019-04-18 17:49:00 +0800

    first commit

---
+++ config/config.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Winter
+ * Date: 2018/9/29
+ * Time: 15:10
+ */
+
+//商户号
+$merchantId = "17034";
+
+//秘钥
+$signKey = "vsbwwoklsy89yywl";
---
+++ controller/notify.php
@@ -0,0 +1,37 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Winter
+ * Date: 2018/9/29
+ * Time: 14:58
+ */
+include '../config/config.php';
+
+$orderAmount = $_POST['orderAmount'];
+$prdOrdNo = $_POST['prdOrdNo'];
+$notifyUrl = $_POST['notifyUrl'];
+
+$data = [
+    'trade_no'      => "randNumber123456",
+    'app_id'        => $merchantId,
+    'out_trade_no'  => $prdOrdNo,
+    'status'        => "success",
+    'way'           => "verify",
+    'amount'        => $orderAmount
+];
+
+$tmp = "trade_no=$data[trade_no]&app_id=$data[app_id]&out_trade_no=$data[out_trade_no]&status=$data[status]&way=$data[way]&amount=$data[amount]&key=".$signKey;
+$data['sign'] = strtolower(md5($tmp));
+
+//发送回调
+$ch = curl_init();
+curl_setopt($ch,CURLOPT_URL, $notifyUrl);
+curl_setopt($ch, CURLOPT_CUSTOMREQUEST, 'POST');
+curl_setopt($ch, CURLOPT_HEADER, false);
+curl_setopt($ch, CURLOPT_POSTFIELDS, http_build_query($data));
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, false);
+$response = curl_exec($ch);
+
+echo $response;
